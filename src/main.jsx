83070d050cef55f661e557f5482449cc805e837a
Oleksandra K <a.koroliova@gmail.com>
2024-05-15 16:34:23 +0300

    Remove HelmetProvider from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
 import { BrowserRouter } from "react-router-dom";
-import { HelmetProvider } from "react-helmet-async";
+import { PersistGate } from "redux-persist/integration/react";
+import { Provider } from "react-redux";
 import App from "./components/App.jsx";
 import { store, persistor } from "./redux/store";
 import "modern-normalize";
@@ -14,9 +13,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter>
-          <HelmetProvider>
-            <App />
-          </HelmetProvider>
+          <App />
         </BrowserRouter>
       </PersistGate>
     </Provider>
