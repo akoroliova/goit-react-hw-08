f0ceb65232900545d122bfb1f4a207a181d8e30f
akoroliova <a.koroliova@gmail.com>
2024-05-18 12:11:14 +0300

    Remove inline styles

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,22 +1,10 @@
-const styles = {
-  container: {
-    minHeight: "calc(100vh - 50px)",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  title: {
-    fontWeight: 500,
-    fontSize: 48,
-    textAlign: "center",
-  },
-};
+import css from "./HomePage.module.css";
 
 export default function HomePage() {
   return (
     <>
-      <div style={styles.container}>
-        <h1 style={styles.title}>
+      <div style={css.container}>
+        <h1 style={css.title}>
           Phonebook welcome page{" "}
           <span role="img" aria-label="Greeting icon">
             💁‍♀️
